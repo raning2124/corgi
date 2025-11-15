7ea8af6947383506f32fc26549c822dfd72f03c8
郭峰 <gfengv1@163.com>
2025-11-15 17:44:36 +0800

    corgi项目初始化

---
+++ corgi-auth/src/main/java/cn/guo/corgi/auth/CorgiAuthApplication.java
@@ -0,0 +1,12 @@
+package cn.guo.corgi.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorgiAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CorgiAuthApplication.class, args);
+    }
+}
