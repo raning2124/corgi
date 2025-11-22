4889f83b9451ff7b3d7631690a575588438c14a2
郭峰 <gfengv1@163.com>
2025-11-22 18:26:49 +0800

    feat: logback配置

---
+++ corgi-common/corgi-common-core/src/main/java/cn/guo/corgi/common/core/App.java
@@ -0,0 +1,13 @@
+package cn.guo.corgi.common.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
--- corgi-gateway/src/main/java/cn/guo/corgi/gateway/web/EchoController.java
+++ corgi-gateway/src/main/java/cn/guo/corgi/gateway/web/EchoController.java
@@ -4,12 +4,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.nio.file.Paths;
+
 @RestController
 public class EchoController {
 
-    @GetMapping("{message}")
+    @GetMapping("/api/{*message}")
     public String echo(@PathVariable String message) {
-        return "Hello: " + message;
+        String fileName = Paths.get(message).getFileName().toString();
+        return "Hello: " + fileName;
     }
 
 }
